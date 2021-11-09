2e4291ad63c5e9c439f1a9fd68a0e25424283de2
AlexDum-dev <alexandre.dumarquez@gmail.com>
2021-11-09 08:13:36 +0100

    Refactorin

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,23 +1,3 @@
-function rechercher() {
-    console.log("first call");
-    var research_value = document.getElementById("search-value").value;
-    
-    //Ecrire la requete en dur
-    var query = `select ?s ?ns where{
-        <%res%> a dbo:Automobile;dbo:successor+ ?s.
-        ?s rdfs:label ?ns
-        FILTER(langMatches(lang(?ns),"EN"))
-        }`;
-
-    var array = [["res", research_value]];
-    getResult(query,array, function (res) {
-        console.log(res);
-    });
-}
-
-
-
-//Replace parameters of a generic query by the variables given in the array
 function getResult(query,array,processResult) {
     for (var i = 0; i<array.length; i++)
     {
@@ -42,15 +22,5 @@ function getResult(query,array,processResult) {
     };
     xmlhttp.open("GET", url + "?" + params, true);
     xmlhttp.send();
-
-
     
-}
-
-function afficherResultats(data) {
-    if (data.results.bindings.length == 0)
-    {
-        //Error not found
-        return;
-    }
 }
