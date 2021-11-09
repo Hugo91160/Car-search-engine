61a3cfb57a7357149ed47ed3f3808bcd353e6ed9
AlexDum-dev <alexandre.dumarquez@gmail.com>
2021-11-09 09:55:41 +0100

    Ajout du script pour la requête car info

---
+++ src/js/carInformation.js
@@ -0,0 +1,35 @@
+function search_car_information(){
+    console.log("Call of search basic car information")
+    var resource = search_resource_name(get_car_info)
+
+}
+
+function get_car_info(res) {
+
+    //Request : 
+    var array = [["val", res]];
+    var query = `SELECT  ?label ?brand ?abstract ?length ?height ?weight ?imagelink ?carname ?class (GROUP_CONCAT(?year, ", ") AS ?prod)
+        WHERE 
+        {
+            OPTIONAL {?carname rdfs:label ?label.}
+            OPTIONAL { ?carname dbo:thumbnail ?imagelink.}
+            OPTIONAL { ?carname dbp:production ?year.}
+            OPTIONAL { ?carname dbp:manufacturer ?brand.}
+            OPTIONAL{ ?carname dbo:abstract ?abstract .}
+            OPTIONAL{ ?carname dbp:class ?class .}
+            OPTIONAL {?carname dbo:length ?length.}
+            OPTIONAL {?carname dbo:height ?height.}
+            OPTIONAL {?carname dbo:weight ?weight.}
+            
+            FILTER(?carname = <%val%> && lang(?label) = "en" && lang(?abstract) = "en")
+        }
+        GROUP BY ?imagelink ?label ?carname ?abstract ?brand ?class ?length ?height ?weight
+    `;
+
+    getResult(query, array,displayCarInfo)
+   
+}
+
+function displayCarInfo(data){
+    //affichage data
+}   
---
+++ src/js/getResource.js
@@ -0,0 +1,28 @@
+function search_resource_name(processResult){
+    //var research_value = document.getElementById("search-value").value;
+    var research_value = "PorScHe 911"
+    console.log(research_value)
+
+    var array = [["val", research_value]];
+    var query = `select distinct ?s ?n 
+    where{ 
+        {{?s a dbo:Automobile.}
+        UNION
+        {?x a dbo:Automobile; dbo:manufacturer ?s.}}
+        ?s rdfs:label ?n.
+        FILTER(langMatches(lang(?n),"EN") && regex(?n, "^%val%$", "i"))
+    }
+    `;
+
+    getResult(query,array, function (data) {
+        //Pre traitement
+        if(data.results.bindings.length != 0){
+            res = data.results.bindings[0].s.value
+            processResult(res);
+        } else {
+            res = undefined
+        }
+        
+    });
+
+}
