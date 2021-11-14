bf4dddd2f2fa5597604caba9a4eeb6271c3e4ee1
tserrania <tserrania@gmail.com>
2021-11-14 21:21:17 +0100

    Fix return button

--- src/js/displayReturnButton.js
+++ src/js/displayReturnButton.js
@@ -11,14 +11,6 @@ function display_return_button (active){
 
 function return_to_table (){
 	display_return_button(false);
-	profile = document.getElementById("profile_result");
-	profile.innerHTML = "";
-	profile = document.getElementById("car_profile");
-	profile.style.display = "none";
-	table = document.getElementById("results-table");
-	table.style.display = "";
-    var pred = document.getElementById("pred_result");
-	pred.style.display = 'none';
-    var succ = document.getElementById("succ_result");
-	succ.style.display = 'none';
+	wipe_car_profile();
+	document.getElementById("brand-section").style.display = "";
 }
